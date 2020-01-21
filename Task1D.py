2ae785c52e9969de8e6bfc2f2d7ed11c22c4f768
Toby Boyne <tobyboyne@gmail,.com>
2020-01-21 13:46:19 +0000

    Added code to Task1D to print the names of stations on a given list of rivers

--- Task1D.py
+++ Task1D.py
@@ -7,5 +7,12 @@ def run():
     print("Number of rivers: {}".format(len(rivers)))
     print("First ten rivers: {}".format(sorted(rivers)[:10]))
 
+    rivers_dict = stations_by_river(stations)
+    test_rivers = ("River Aire", "River Cam", "River Thames")
+    for river in test_rivers:
+        stations_on_river = rivers_dict.get(river)
+        station_names = [s.name for s in stations_on_river]
+        print("Stations on {}: {}".format(river, station_names))
+
 if __name__ == "__main__":
     run()
