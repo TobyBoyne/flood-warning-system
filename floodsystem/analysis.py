a2a7a940e3b338fdbd224d6766ecbc4983add338
Toby Boyne <tobyboyne@gmail,.com>
2020-02-21 19:44:06 +0000

    Created analysis.py, wrote polyfit function

---
+++ floodsystem/analysis.py
@@ -0,0 +1,14 @@
+from matplotlib.dates import date2num
+import numpy as np
+
+def polyfit(dates, levels, p):
+    """Calculate the polynomial of order p that passes through the data points
+    Returns a poly1d object, and the time axis offset"""
+    date_nums = date2num(dates)
+    # d0 is the shift of the time axis - first date
+    d0 = dates[0]
+
+    p_coeff = np.polyfit(date_nums, levels, p)
+    poly = np.poly1d(p_coeff)
+
+    return poly, d0
