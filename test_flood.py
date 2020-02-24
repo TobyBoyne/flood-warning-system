d2d481c1fd97cb63ade5b969fab67be1f4cd45c8
Polina Abrosimova <pa443@cam.ac.uk>
2020-02-24 11:40:54 +0000

    Created test_flood

---
+++ test_flood.py
@@ -0,0 +1,7 @@
+from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
+
+def test_stations_highest_rel_level():
+    pass
+
+def test_stations_level_over_threshhold():
+    pass
