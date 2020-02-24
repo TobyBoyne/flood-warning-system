8ba1f5776a76a30be73c543ed0cea7caee12e42b
Toby Boyne <tobyboyne@gmail,.com>
2020-02-24 00:10:12 +0000

    Created Task2G

---
+++ Task2G.py
@@ -0,0 +1,11 @@
+from floodsystem.analysis import projected_level_after_dt
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list
+
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
+    run()
